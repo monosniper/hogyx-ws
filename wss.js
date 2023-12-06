18d45554b23ecfb70c1ffe80e428e56a7545310d
monosniper <monosniper@gmail.com>
2023-12-06 05:18:39 +0500

    asd

--- wss.js
+++ wss.js
@@ -10,7 +10,7 @@ const server = createServer({
     cert: readFileSync('server.crt'),
     key: readFileSync('server.key')
 });
-const wss = new WebSocketServer({server});
+const wss = new WebSocketServer({ noServer: true });
 
 const send = (ws, message, data = {}) => ws.send(JSON.stringify({
     message, data
