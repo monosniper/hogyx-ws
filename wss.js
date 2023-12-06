55177b37e05b10beca24b0e7c8c60ab361c1c77a
monosniper <monosniper@gmail.com>
2023-12-06 16:10:32 +0500

    dasd

--- wss.js
+++ wss.js
@@ -1,16 +1,16 @@
 const {WebSocketServer} = require("ws");
-const {createServer} = require("http");
-// const {createServer} = require("https");
+// const {createServer} = require("http");
+const {createServer} = require("https");
 const api = require("./api");
 const {check_interval} = require("./config");
 const Miner = require("./miner");
 const {readFileSync} = require("fs");
 
-const server = createServer()
-// const server = createServer({
-//     cert: readFileSync('server.crt'),
-//     key: readFileSync('server.key')
-// });
+// const server = createServer()
+const server = createServer({
+    cert: readFileSync('server.crt'),
+    key: readFileSync('server.key')
+});
 const wss = new WebSocketServer({ noServer: true });
 
 const send = (ws, message, data = {}) => ws.send(JSON.stringify({
