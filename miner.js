26f74f0ebc6e4e9eb2368711209c7d92e6194a8d
monosniper <monosniper@gmail.com>
2023-12-29 22:44:20 +0500

    asd

--- miner.js
+++ miner.js
@@ -167,7 +167,7 @@ class Miner {
             console.log("logs count: " + this.current_server.logs.length)
             console.log("founds count: " + this.current_server.founds.length)
 
-            if(this.current_server.logs.length <= 3600) {
+            if(this.current_server.logs.length <= 900) {
                 this.update({
                     logs: this.current_server.logs,
                     founds: this.current_server.founds,
@@ -175,7 +175,7 @@ class Miner {
                     if(rs) this.servers_updated[this_server_id] = rs.success
                 })
             } else {
-                const chunkSize = 1800;
+                const chunkSize = 900;
                 let data = {
                     logs: [],
                     founds: [],
@@ -190,7 +190,7 @@ class Miner {
 
                 nextChunk(0)
 
-                if(this.current_server.founds.length <= 3600) {
+                if(this.current_server.founds.length <= 900) {
                     this.update({
                         logs: [],
                         founds: this.current_server.founds,
